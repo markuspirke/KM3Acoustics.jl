dbe2fc2c3ba19097a261bab5961c1fe49b8b49a2
Markus Pirke <markus.pirke@web.de>
2022-09-14 12:11:48 +0200

    added some documentation (not complete yet)

--- src/acoustics.jl
+++ src/acoustics.jl
@@ -7,7 +7,13 @@ const f_s = 195312.5 #sampling frequency
 #frame_length = DAQ_ADF_ANALYSIS_WINDOW_SIZE - DAQ_ADF_ANALYSIS_WINDOW_OVERLAP #check whether this can be hard coded from the beginning or whether these values change
 frame_length = 123260
 l = frame_length + 3 #number of 4 Bytes in the whole file
-
+"""
+ASignal is a custom type with four fields to store all the information inside the raw acoustic binary files.
+- utc_seconds:: UInt32 storing the first 4 Bytes and is a UNIX time stamp
+- ns_cycles:: UInt32 storing the second 4 Bytes
+- samples:: UInt32 storing the third 4 Bytes, corresponding to the number of data points accuired during the measring window
+- pcm:: Vector of Float32 of length frame_length, storing all other 4 Byte blocks. Each entry is a data point of the acoustic signal.
+"""
 struct ASignal
     #dom_id::String add if necessary
     utc_seconds::UInt32 # UNIX timestamp
@@ -15,7 +21,11 @@ struct ASignal
     samples::UInt32 #  as 'samples' corresponds to the frame_length which is apprantely a fixed number 123260 so maybe this isnt necessary
     pcm::Vector{Float32}
 end
-
+"""
+The function read_asignal provides a tool to read in raw acoustic binary files which where recorded by an acoustic detection unit build into the DOMs.
+The function takes two arguments as an input. The first argument is the filename of the binary file and the second argument is the number of 4 Byte snippets in the binary.
+The whole information from the binary file is return inside our custom type ASignal.
+"""
 function read_asignal(filename::AbstractString,fourbyte_length::Int)
 
     container = Vector{UInt32}(undef,3)
@@ -41,7 +51,10 @@ function plot_asignal(signal::ASignal, f_s=f_s, l=frame_length) #this should be 
 
     return p
 end
-
+"""
+The function to_wav takes our custom type ASignal as a first argument and procudes .wav file. The second argument is the path where the .wav file gets stored.
+The third argument is the sampling frequency at which the signal was recorded and with the last argument it is possible to amplify the signal.
+"""
 function to_wav(signal::ASignal, path::AbstractString, f_s=f_s, gain_db=0.0)
     pcm = signal.pcm
 
