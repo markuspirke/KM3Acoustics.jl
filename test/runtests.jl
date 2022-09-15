db7c49e8b472a3974491a011e7783ecff807b182
Tamas Gal <himself@tamasgal.com>
2022-09-15 13:42:00 +0200

    Add more tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,4 +1,4 @@
-import KM3Acoustics: Detector
+import KM3Acoustics: Detector, Location, Quaternion
 using Test
 
 const SAMPLES_DIR = joinpath(@__DIR__, "samples")
@@ -10,7 +10,17 @@ const SAMPLES_DIR = joinpath(@__DIR__, "samples")
             # no base modules in DETX
             @test 342 == length(d.modules)
         else
+            # base module attributes
             @test 361 == length(d.modules)
+            @test 116.600007 ≈ d.modules[808992603].pos.x  # optical module
+            @test 106.95 ≈ d.modules[808469291].pos.y  # base module
+            @test 97.3720395 ≈ d.modules[808974928].pos.z  # base module
+            @test Quaternion(1, 0, 0, 0) ≈ d.modules[808995481].q
+            @test 0.0 ≈ d.modules[808966287].t₀
         end
+
+        @test 31 == d.modules[808992603].n_pmts
+        @test 30 ≈ d.modules[817287557].location.string
+        @test 18 ≈ d.modules[817287557].location.floor
     end
 end
