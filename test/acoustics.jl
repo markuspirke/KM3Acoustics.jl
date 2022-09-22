e7f657359b2667d1ae5c5f559fa62c62fd2c9080
Markus Pirke <markus.pirke@web.de>
2022-09-22 10:55:53 +0200

    forgot to add test/acoustics.jl

---
+++ test/acoustics.jl
@@ -0,0 +1,11 @@
+using KM3Acoustics
+
+@testset "acoustics" begin
+    signal = read(joinpath(SAMPLES_DIR, "DOM_808956920_CH1_1608751683.bin"), ASignal)
+    @test 808956920 == signal.dom_id
+    @test 0x5fe39a3f == signal.utc_seconds
+    @test 0x00020000 == signal.samples
+    @test 123260 == length(signal.pcm)
+    @test 0.0031588078f0 == signal.pcm[1]
+    @test 0.0033951998f0 == signal.pcm[end]
+end
