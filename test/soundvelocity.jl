aeec1cac15e882b22f0b5d9de2408b22f3f4530d
Markus Pirke <markus.pirke@web.de>
2022-09-22 15:08:07 +0200

    changed soundvelocity.jl and added tests

--- test/soundvelocity.jl
+++ test/soundvelocity.jl
@@ -1,11 +1,20 @@
 using KM3Acoustics
+using Test
 
 @testset "soundvelocity" begin
-    @test SoundVelocity(1541.0, -2000.0) == get_velocity(-2000.0)
-    @test SoundVelocity(1558.0, -3000.0) == get_velocity(-3000.0)
+    @test SoundVelocity(1541.0, -2000.0) == velocity(-2000.0)
+    @test SoundVelocity(1558.0, -3000.0) == velocity(-3000.0)
 
-    tripod = Tripod(1,Position(1000.0,1000.0,-3000.0))
-    @test SoundVelocity(1558.0, -3000.0) == get_velocity(tripod)
+    tripod = Tripod(1,Position(0.0,0.0,-3000.0))
+    @test SoundVelocity(1558.0, -3000.0) == velocity(tripod)
 
+    mod = DetectorModule(1, Position(100.0,0.0,-3000.0), Location(0, 0), 0, PMT[], missing, 0, missing)
+    @test SoundVelocity(1558.0, -3000.0) == velocity(mod)
+
+    mod1 = DetectorModule(1, Position(0.0,0.0,-2000.0), Location(0, 0), 0, PMT[], missing, 0, missing)
+    @test 0.06418485237483953 ≈ traveltime(tripod, mod)
+    @test 0.06418485237483953 ≈ traveltime(mod, tripod)
+    @test 0.6453759476810511 ≈ traveltime(tripod, mod1)
+    @test 0.6453759476810511 ≈ traveltime(mod1, tripod)
     # test of get_veloctiy(module) and get_time missing
 end
