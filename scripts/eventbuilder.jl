4ee78e0f3c6fa3d9bd8ca7bca2f6b82586d8b5ef
Markus Pirke <markus.pirke@web.de>
2022-09-29 16:23:13 +0200

    eventbuilder script update docs

--- scripts/eventbuilder.jl
+++ scripts/eventbuilder.jl
@@ -50,19 +50,19 @@ function main()
 
     receivers = Dict{Int32, Receiver}()
     emitters = Dict{Int8, Emitter}()
-    for tripod ∈ tripods
+    for tripod ∈ tripods # change position of tripods from .txt file to relative position of the detector
         emitters[tripod.id] = Emitter(tripod.id, tripod.pos - detector.pos)
     end
     hydrophones1 = Dict{Int32, Hydrophone}()
-    for hydrophone ∈ hydrophones
+    for hydrophone ∈ hydrophones # makes a dictionary of hydrophones, with string number as keys
         hydrophones1[hydrophone.location.string] = hydrophone
     end
 
-    for (module_id, mod) ∈ detector.modules
-        if (mod.location.floor == 0 && hydrophoneenabled(mod)) || (mod.location.floor != 0 && piezoenabled(mod))
+    for (module_id, mod) ∈ detector.modules # go through all modules and check whether they are base modules and have hydrophone
+        if (mod.location.floor == 0 && hydrophoneenabled(mod)) || (mod.location.floor != 0 && piezoenabled(mod)) #or they are no base module and have piezo
             pos = Position(0, 0, 0)
-            if mod.location.floor == 0
-               pos += hydrophones1[mod.location.string].pos
+            if mod.location.floor == 0 # if base module and hydrophone
+               pos += hydrophones1[mod.location.string].pos # position in of hydrophone relative to T bar gets added
             end
             pos += mod.pos
             receivers[module_id] = Receiver(module_id, pos)
@@ -71,7 +71,7 @@ function main()
 
     DD = DefaultDict{Int32, DefaultDict}(DefaultDict{Int32, Vector{Transmission}}(Vector{Float64}))
 
-    for row ∈ eachrow(toashort)
+    for row ∈ eachrow(toashort) # checks whether signal from toashort fits to some module and tripod
         emitter_id = waveforms.ids[row.EMITTERID] # transform EMITTERID from toashort to tripod ID
         if (haskey(receivers, row.DOMID)) && (haskey(emitters, emitter_id)) #check if DOM and Tripod is in detector
             toe = row.UTC_TOA - traveltime(receivers[row.DOMID], emitters[emitter_id]) #calculate TOE
@@ -84,9 +84,8 @@ function main()
 
     for (emitter_id, receivers) ∈ DD #filter out similar events
        for (receiver_id, transmissions) ∈ receivers
-           sort!(transmissions)
+           sort!(transmissions) # sort by first TOA, if TOA equal, the higher Q factor comes first
            unique!(x -> x.TOA, transmissions)
-           # sort!(transmissions, by = x -> x.TOE) # at last sort the transmissions by TOE
         end
     end
 
@@ -99,19 +98,19 @@ function main()
     for (emitter_id, receivers) ∈ DD
         transmissions_buffer = Transmission[]
         for (receiver_id, transmissions) ∈ receivers #there is still some statistics missing
-            transmissions_buffer = vcat(transmissions_buffer, transmissions)
+            transmissions_buffer = vcat(transmissions_buffer, transmissions) #all signal from one emitter go back into one vector
         end
-        sort!(transmissions_buffer, by = x -> x.TOE)
+        sort!(transmissions_buffer, by = x -> x.TOE) # sort according to time of emission
         buffer_length = length(transmissions_buffer)
 
-        for (i, transmission) ∈ enumerate(transmissions_buffer)
+        for (i, transmission) ∈ enumerate(transmissions_buffer) # go through all signals
             j = copy(i)
             j += 1
             while (j <= buffer_length) && (transmissions_buffer[j].TOE - transmission.TOE <= trigger.tmax)
-                j += 1
+                j += 1 # group signal during a certain kind of time intervall
             end
             k = j - i + 1 #events in time frame
-            if k >= trigger.nmin
+            if k >= trigger.nmin #if more then 90 signal during tmax write down the event
                push!(events, Event(detector.id, k, emitter_id, transmissions_buffer[i:j]))
             end
         end
