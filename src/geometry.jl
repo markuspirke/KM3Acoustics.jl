32b8c16063668d5fe2063bca74dfdaffb17a5cb2
Markus Pirke <markus.pirke@web.de>
2022-10-30 12:23:38 +0100

    restructure of geometry, implented curvature

--- src/geometry.jl
+++ src/geometry.jl
@@ -3,37 +3,95 @@ struct ToyString
     pos::Position #position of the tbar at the seabed
     θ::Float64 # polar angle
     ϕ::Float64 # azimuthal angle
-    h₀::Float64 # height from base to first module
-    h::Float64 # height of a floor
+    lengths::Vector{Float64} # height of floors or length of the string measured from base
 end
-
 struct ToyModule
-    id::Int32 #why ID ???
     location::Location
     pos::Position
+end
+struct RealString
+    id::Int32
+    pos::Position
+    dx::Float64
+    dy::Float64
+    lengths::Vector{Float64}
 end
 
 struct ToyDetector
     strings::Dict{Int32, ToyString}
 end
 
-function toy_position(θ, ϕ, j, toystring) # x = [ϕ, θ], j floor
-    if j == 0
-        ToyModule(1, Location(toystring.id, j), toystring.pos)
-    elseif j == 1
-        x = toystring.pos + toystring.h₀ * j * Position(sin(θ)*cos(ϕ), sin(θ)*sin(ϕ), cos(θ))
-        ToyModule(1, Location(1,j), x)
-    else
-        x = toystring.pos + (toystring.h * (j-1) + toystring.h₀) * Position(sin(θ)*cos(ϕ), sin(θ)*sin(ϕ), cos(θ))
-        ToyModule(1, Location(1,j), x)
-    end
+struct RealDetector
+    strings::Dict{Int32, RealString}
+end
+"""
+    function toy_calc_pos(θ, ϕ, l)
+
+Given the length of the string up to the module, returns the position of the module,
+relative to the basemodule
+"""
+function toy_calc_pos(θ, ϕ, l)
+    Position(l*sin(θ)*cos(ϕ), l*sin(θ)*sin(ϕ), l*cos(θ))
+end
+"""
+    function toy_calc_toa(θ, ϕ, l, basepos, emitterpos)
+
+Given the length of the string up to the module, the angles of the strings,
+returns the traveltime between the given module and the emitter.
+"""
+function toy_calc_traveltime(θ, ϕ, l, basepos, emitterpos)
+
+    modpos = toy_calc_pos(θ, ϕ, l) + basepos
+    R = norm(modpos - emitterpos)
+    traveltime(R, modpos.z, emitterpos.z, -2440.0)
 end
 
-function toy_toa(p, j, tripod, toystring)
-    t, ϕ, θ = p
+"""
+    function string_length(dx, dy, z, a, b)
 
-    mod = toy_position(ϕ, θ, j, toystring)
-
-    toa = t + traveltime(tripod, mod, -2440.0)
+Given the height of a module located in some string, calculates the length of the string
+from the basemodule to the module.
+"""
+function string_length(dx, dy, z, a, b)
+    sqrt(1 + (dx^2 + dy^2)) * z + 0.5*(dx^2 + dy^2) * b*log(1 - a*z)
 end
 
+function string_inverselength(dx, dy, l, a, b)
+    z = find_zero(x -> string_length(dx, dy, x, a, b) - l, l)
+end
+
+function string_inverselength1(dx, dy, l, a, b)
+    fx = ZeroProblem(x -> string_length(dx, dy, x, a, b) - l, l)
+    solve(fx)
+end
+
+# function string_inverselength2(dx, dy, l, a, b)
+#     probN = NonlinearProblem{false}(x -> string_length(dx, dy, x, a, b) - l, l)
+#     solver = solve(probN, NewtonRaphson(), tol=1e-6)
+# end
+"""
+    function calc_traveltime(dx, dy, l, a, b, basepos, emitterpos)
+
+Calculates the position of a module located an some string at length l measured from the
+basemodule.
+"""
+function calc_pos(dx, dy, l, a, b)
+    z = string_inverselength(dx, dy, l, a, b)
+    x = dx * z
+    y = dy * z
+
+    Position(x, y, z)
+end
+"""
+    function calc_traveltime(dx, dy, l, a, b, basepos, emitterpos)
+
+Calculates the traveltime of a signal of an emitter to a module located at
+the position length l at some string.
+"""
+function calc_traveltime(dx, dy, l, a, b, basepos, emitterpos)
+
+    modpos = calc_pos(dx, dy, l, a, b) + basepos
+    R = norm(modpos - emitterpos)
+    traveltime(R, modpos.z, emitterpos.z, -2440.0)
+    # traveltime(modpos, emitterpos, -2440.0, SoundVelocity(1541.0, -2000.0))
+end
--- test/geometry.jl
+++ test/geometry.jl
@@ -2,9 +2,18 @@ using KM3Acoustics
 using Test
 
 @testset "geometry" begin
-    toystring = ToyString(1, Position(0.0, 0.0, 0.0), 0.0, 0.0, 30.0, 10.0)
-    @test ToyModule(1, Location(1, 0), Position(0.0, 0.0, 0.0)) == toy_position(0.0, 0.0, 0, toystring)
-    @test ToyModule(1, Location(1, 1), Position(0.0, 0.0, 30.0)) == toy_position(0.0, 0.0, 1, toystring)
-    @test ToyModule(1, Location(1, 2), Position(0.0, 0.0, 40.0)) == toy_position(0.0, 0.0, 2, toystring)
-    @test 0.025837456976183806 ≈ toy_toa([0.0, 0.0, 0.0], 2, Emitter(1, Position(0.0, 0.0, 0.0)), toystring)
+    toystring = ToyString(1, Position(0.0, 0.0, 0.0), 0.0, 0.0, [30.0, 40.0, 50.0])
+    @test Position(0.0, 0.0, 0.0) == toy_calc_pos(0.0, 0.0, 0.0)
+    @test Position(0.0, 0.0, 30.0) == toy_calc_pos(0.0, 0.0, toystring.lengths[1])
+    @test 0.06461493383755239 ≈ toy_calc_traveltime(0.0, 0.0, 100.0, Position(0.0, 0.0, 0.0), Position(0.0, 0.0, 0.0))
+    a = 0.00311
+    b = 85.1
+    dx = 0.1
+    dy = 0.1
+    @test 100.6780399628396 ≈ string_length(dx, dy, 100.0, a, b)
+    @test 100.0 ≈ string_inverselength(dx, dy ,  100.6780399628396, a, b)
+    @test Position(0.0, 0.0, 100.0) ≈ calc_pos(0.0, 0.0, 100.0, 0.0, 0.0)
+    @test Position(10.0, 10.0, 100.0) ≈ calc_pos(0.1, 0.1, 100.6780399628396, a, b)
+    @test 0.06525788433842991 ≈ calc_traveltime(dx, dy, 100.6780399628396, a, b, Position(0.0,0.0,0.0), Position(0.0,0.0,0.0))
+
 end
