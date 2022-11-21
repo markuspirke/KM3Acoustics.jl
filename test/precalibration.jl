44f407ba177cdd78c3a33720c3fe90f84706d8fd
Markus Pirke <markus.pirke@web.de>
2022-11-21 13:16:25 +0100

    added new tests for precalibration

--- test/precalibration.jl
+++ test/precalibration.jl
@@ -12,20 +12,21 @@ const SAMPLES_DIR = joinpath(@__DIR__, "samples")
     @test 30 == collect(keys(hydrophones))[end]
     tripods = read(joinpath(SAMPLES_DIR, "tripod.txt"), Tripod)
     emitters = tripod_to_emitter(tripods, detector)
-    pc = Precalibration(detector.pos, hydrophones, 10, events, emitters)
+
+    pc = Precalibration(detector.pos, hydrophones, 10, events, emitters, numevents=3)
     @test Position(587600.0, 4.0168e6, -3450.0) ≈ pc.detector_pos
     @test 15 == length(pc.hydrophones)
     @test 10 == collect(keys(pc.hydrophones))[1]
     @test 30 == collect(keys(pc.hydrophones))[end]
-    @test 8 == pc.nevents
-    @test 3 == collect(keys(pc.emitters))[1]
-    @test 10 == collect(keys(pc.emitters))[end]
+    @test 6 == pc.nevents
+    @test 7 == collect(keys(pc.emitters))[1]
+    @test 13 == collect(keys(pc.emitters))[end]
     p0 = precalibration_startvalues(pc)
-    @test 65 == length(p0)
+    @test 63 == length(p0)
     @test 1.6643716922418897e9 ≈ p0[end]
     @test -137.46 ≈ p0[1]
-    @test 1.4789626721357605e8 ≈ pc(p0)
-    @test 1.4789626721357605e8 ≈ pc(p0...)
+    @test -2.37144897891234e7 ≈ pc(p0)
+    @test 3.6103302860852994e7 ≈ pc(p0...)
     @test emitters == get_opt_emitters(pc, p0)
     @test hydrophones == get_opt_hydrophones(pc, p0)
 end
