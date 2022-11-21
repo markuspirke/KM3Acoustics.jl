9867a81f9aa56e73cd63c1e4bac48904d04bba4e
Markus Pirke <markus.pirke@web.de>
2022-11-21 15:32:06 +0100

    updated tests for precalibration

--- test/precalibration.jl
+++ test/precalibration.jl
@@ -4,35 +4,51 @@ using Test
 const SAMPLES_DIR = joinpath(@__DIR__, "samples")
 
 @testset "precalibration" begin
-    detector = Detector(joinpath(SAMPLES_DIR, "v5.detx"))
+    detector = Detector(joinpath(SAMPLES_DIR, "KM3NeT_00000133_00013346.detx"))
     events = read_events(joinpath(SAMPLES_DIR, "KM3NeT_00000133_00013346_preevent.h5"), 13346)
     hydrophones = get_hydrophones(joinpath(SAMPLES_DIR, "hydrophone.txt"), detector, events)
     @test 15 == length(hydrophones)
     @test 10 == collect(keys(hydrophones))[1]
     @test 30 == collect(keys(hydrophones))[end]
-    tripods = read(joinpath(SAMPLES_DIR, "tripod.txt"), Tripod)
-    emitters = tripod_to_emitter(tripods, detector)
+    emitters = read(joinpath(SAMPLES_DIR, "tripod.txt"), Emitter, detector)
+    fixhydro = [(10, :x), (10, :y), (10, :z)]
+    fixemitter = []
+    pc = Precalibration(detector.pos, events, hydrophones, fixhydro, emitters, fixemitter, rotate=0, nevents=2)
+    @test 72 == length(pc.p0s)
+    @test -137.45999999999998 ≈ pc.p0s[1]
+    ps = unwrap(pc.p0s, pc)
+    pos_hydro, pos_emitter, toes = split_p(ps, pc)
+    @test Position(52.77, 180.185, 2.420014848519585) == pos_hydro[1]
+    @test Position(-401.3719999999739, -571.3070000000298, 16.69399999999996) == pos_emitter[1]
+    @test toes[1] == eventtime(pc.events[7][1])
+    @test 2 == length(pc.events[7])
+    pc1 = Precalibration(detector.pos, events, hydrophones, fixhydro, emitters, fixemitter, rotate=0, nevents=2, mask=2:2)
+    @test 1 == length(pc1.events[7])
+    @test toes[2] == eventtime(pc1.events[7][1])
 
-    pc = Precalibration(detector.pos, hydrophones, 10, events, emitters, numevents=3)
-    @test Position(587600.0, 4.0168e6, -3450.0) ≈ pc.detector_pos
-    @test 15 == length(pc.hydrophones)
-    @test 10 == collect(keys(pc.hydrophones))[1]
-    @test 30 == collect(keys(pc.hydrophones))[end]
-    @test 6 == pc.nevents
-    @test 7 == collect(keys(pc.emitters))[1]
-    @test 13 == collect(keys(pc.emitters))[end]
-    p0 = precalibration_startvalues(pc)
-    @test 63 == length(p0)
-    @test 1.6643716922418897e9 ≈ p0[end]
-    @test -137.46 ≈ p0[1]
-    @test -2.37144897891234e7 ≈ pc(p0)
-    @test 3.6103302860852994e7 ≈ pc(p0...)
-    @test emitters == get_opt_emitters(pc, p0)
-    @test hydrophones == get_opt_hydrophones(pc, p0)
+
+    # pc = Precalibration(detector.pos, events, hydrophones, fixhydro, emitters, fixemitter, nevent=5)
+
+    # pc = Precalibration(detector.pos, hydrophones, 10, events, emitters, numevents=3)
+    # @test Position(587600.0, 4.0168e6, -3450.0) ≈ pc.detector_pos
+    # @test 15 == length(pc.hydrophones)
+    # @test 10 == collect(keys(pc.hydrophones))[1]
+    # @test 30 == collect(keys(pc.hydrophones))[end]
+    # @test 6 == pc.nevents
+    # @test 7 == collect(keys(pc.emitters))[1]
+    # @test 13 == collect(keys(pc.emitters))[end]
+    # p0 = precalibration_startvalues(pc)
+    # @test 63 == length(p0)
+    # @test 1.6643716922418897e9 ≈ p0[end]
+    # @test -137.46 ≈ p0[1]
+    # @test -2.37144897891234e7 ≈ pc(p0)
+    # @test 3.6103302860852994e7 ≈ pc(p0...)
+    # @test emitters == get_opt_emitters(pc, p0)
+    # @test hydrophones == get_opt_hydrophones(pc, p0)
 end
 
 @testset "statistics" begin
     events = read_events(joinpath(SAMPLES_DIR, "KM3NeT_00000133_00013346_preevent.h5"), 13346)
-    @test 1.664371042252927e9 ≈ mean(events[1])
-    @test 0.0023154017972801906 ≈ std(events[1])
+    @test 1.6643707765755563e9 ≈ mean(events[1], :TOE)
+    @test 0.0037644351140815745 ≈ std(events[1], :TOE)
 end
