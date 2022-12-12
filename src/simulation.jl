203f0fe59073d99f839cd09ff05fbfcc9f51e21d
Markus Pirke <markus.pirke@web.de>
2022-12-12 10:10:04 +0100

    added documentation to simulation.jl and statistics.jl

--- src/simulation.jl
+++ src/simulation.jl
@@ -1,7 +1,9 @@
 """
     function inverse_waveforms(waveform::Waveform)
 
-Return an inverse ( not injective ) map of the waveform.txt file.
+Return an inverse ( injective but not bijective ) map of the waveform.txt file. The mapping is not bijective because
+there are more Waveform IDs then tripod IDs. In this inverse function I leave out some waveform IDs
+and just map all IDs from one tripod to one waveform ID. 
 """
 function inverse_waveforms(waveform::Waveform)
     D = Dict{Int8,Int8}()
@@ -10,7 +12,14 @@ function inverse_waveforms(waveform::Waveform)
     end
     Waveform(D)
 end
-
+"""
+    function simulation_times(t0, emitters)
+    
+Given a time t0, where one wants to start the simulation, and some emitters,
+this function returns a tuple of vectors. Each tuple consists of an emitter ID
+and a starting time for an acoustic event. The times are randomly selected and are 
+seperated by a time window of 20 seconds. 
+"""
 function simulation_times(t0, emitters)
     xs = Tuple{Int8,Float64}[]
     randemitterids = shuffle(collect(keys(emitters)))
@@ -20,7 +29,15 @@ function simulation_times(t0, emitters)
     end
     xs
 end
+"""
+    function signal_impulses(t0)
 
+Given a certain time t0 where the acoustic events starts,
+this function returns a vector of times, each seperated
+by 5 seconds. The vector stores 11 time points, because
+a Beacon on the seafloor emitts a series of 11 acoustic
+signals every time it sends out a signal. 
+"""
 function signal_impulses(t0)
     ts = Float64[]
     for i in 0:10 # there are 11 impules during one emission of a signal from a beacon
@@ -32,28 +49,55 @@ end
     function acoustic_event(detector, emitter, receivers, toe, run; p=0.8)
 
 Simulates an acoustic event from one emitter to the receivers. The receiver gets the signal with a probability
-of p. Returns a RawToashort.
+of p. Returns a RawToashort. As an additional option one can simulates acoustic events with a given error.
+Which can be given for example as: error = Normal(0, error) where Normal is a normal distribution from
+Distribution.jl. 
 """
-function acoustic_event(detector, emitter, invwaveform, receivers, toe, run; p=1.0)#, error=Normal(0.0, 50e-6))
+function acoustic_event(detector, emitter, invwaveform, receivers, toe, run; p=1.0, error=0)
     emitterwaveform = invwaveform.ids[emitter.id]
     toashorts = RawToashort[]
-    for receiver in values(receivers)
-        if rand() < p
-            t = traveltime(emitter, receiver, detector.pos.z)
-            toa = t + toe #+ rand(error)
-            tshort = RawToashort(run, run, toa, receiver.id, emitterwaveform, 0.0, 0.0) # obda set TOA_S = Q = 0
-            push!(toashorts, tshort)
+    if error == 0
+        for receiver in values(receivers)
+            if rand() < p
+                t = traveltime(emitter, receiver, detector.pos.z)
+                toa = t + toe #+ rand(error)
+                tshort = RawToashort(run, run, toa, receiver.id, emitterwaveform, 0.0, 0.0) # obda set TOA_S = Q = 0
+                push!(toashorts, tshort)
+            end
         end
-    end
+    else
+        for receiver in values(receivers)
+            if rand() < p
+                t = traveltime(emitter, receiver, detector.pos.z)
+                toa = t + toe + rand(error)
+                tshort = RawToashort(run, run, toa, receiver.id, emitterwaveform, 0.0, 0.0) # obda set TOA_S = Q = 0
+                push!(toashorts, tshort)
+            end
+        end
+
     toashorts
 end
+"""
+    function save_rawtoashorts(filename, toashorts, run)
 
+Saves the simulated acoustic events in the same file format as the
+data is stored in the database. However this will return a H5 file not a csv.
+"""
 function save_rawtoashorts(filename, toashorts, run)
     h5open(filename, "w") do h5f
         write_compound(h5f, "toashort/$(run)", toashorts)
     end
 end
-
+"""
+    function mutate_modules(modules::T) where {T<:Array}
+        
+Helper function to change position of a given vector of modules.
+The function will ask in the command line whether you want to 
+add changes in the postition of a certain module. If no changes 
+should be applied type <n> and <enter>. If the position should be 
+changed type <change in x as Float> <spc> <change in y as Float>
+<spc> <change in z as Float> and <enter>.
+"""
 function mutate_modules(modules::T) where {T<:Array}
     nmodules = typeof(modules[1])[]
     for mod in modules
@@ -70,6 +114,16 @@ function mutate_modules(modules::T) where {T<:Array}
     nmodules
 end
 
+"""
+    function mutate_modules(modules::T) where {T<:Dict}
+        
+Helper function to change position of a given dictionary of modules.
+The function will ask in the command line whether you want to 
+add changes in the postition of a certain module. If no changes 
+should be applied type <n> and <enter>. If the position should be 
+changed type <change in x as Float> <spc> <change in y as Float>
+<spc> <change in z as Float> and <enter>.
+"""
 function mutate_modules(modules::T) where {T<:Dict}
     nmodules = typeof(modules)()
     for (k, mod) in modules
--- src/statistics.jl
+++ src/statistics.jl
@@ -1,7 +1,17 @@
+"""
+    function Statistics.mean(event::Event, field::Symbol)
+
+Return the mean of a given field from the data included in the event. 
+"""
 function Statistics.mean(event::Event, field::Symbol)
     mean([getfield(transmission, field) for transmission in event.data])
 end
 
+"""
+    function Statistics.std(event::Event, field::Symbol)
+
+Return the std of a given field from the data included in the event. 
+"""
 function Statistics.std(event::Event, field::Symbol)
     std([getfield(transmission, field) for transmission in event.data])
 end
